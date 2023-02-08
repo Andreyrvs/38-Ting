e98739947ce58e7551d3047ba623e634de0abfbe
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 19:59:56 -0300

    chore: (Requisito 02) - Adiciona Try:except:.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,14 +1,12 @@
 import sys
 
 def txt_importer(path_file):
-    print('path_file: ', path_file)
-    if path_file == "":
-        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    try:
+        if path_file[len(path_file) - 4:] != ".txt":
+            return print(f"Formato inválido", file=sys.stderr)
 
-    if path_file[len(path_file) - 4] != ".txt":
-        return print(f"Formato inválido", file=sys.stderr)
-
-    with open(path_file) as file:
-        lines = file.readlines()
-        for line in lines:
-            return f"{line}\n"
+        with open(path_file) as file:
+            lines = file.read()
+            return lines.split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
