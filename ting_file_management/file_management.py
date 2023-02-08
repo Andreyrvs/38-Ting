ef545cb1c967a543322fb5cda71f4009740d27d7
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 19:49:42 -0300

    Feat: (Requisito 02) - Cria validacao caso o arquivo nao seja um .txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file == "":
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    if path_file[len(path_file) - 3] != "txt":
+        return print(f"Formato inválido", file=sys.stderr)
+    print(f"🌊🌊🌊🌊{path_file}")
+    with open(path_file) as file:
+        lines = file.readlines()
+        for line in lines:
+            print(f"{line}\n")
