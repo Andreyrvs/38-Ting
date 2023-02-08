03d46e8fceb9223c3ed5b839f1b06046f42d2643
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 20:16:28 -0300

    chore: (Requisito 03) - adicona retorno na verificacao do arquivo que esta na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,16 +3,16 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
 
-    for file in range(len(instance)):
-        if instance.search(file)["nome_do_arquivo"] == path_file:
-            return
-
     file = txt_importer(path_file)
     data = {
        "nome_do_arquivo": path_file,
         "qtd_linhas": len(file),
         "linhas_do_arquivo": file,
     }
+
+    for file in range(len(instance)):
+        if instance.search(file)["nome_do_arquivo"] == path_file:
+            return "arquivos iguais na queue"
     instance.enqueue(data)
     return print(data, file=sys.stdout)
 
