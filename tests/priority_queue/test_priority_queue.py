961d5db281a8680eb57af23e80bf0c7561f2eec0
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 20:56:22 -0300

    feat: (Requisito 06) - Inicia os testes da fila de prioridade

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue= PriorityQueue()
+    high_priority = priority_queue.enqueue({"qtd_linhas": 4})
+    regular_priority = priority_queue.enqueue({"qtd_linhas": 14})
+    
+    priority_queue.enqueue(high_priority)
+    priority_queue.enqueue(regular_priority)
+    assert len(priority_queue.regular_priority) == 2
+
+    priority_queue.enqueue(high_priority)
+    priority_queue.enqueue(regular_priority)
+    assert priority_queue.dequeue() == high_priority
+
+    priority_queue.enqueue(high_priority)
+    priority_queue.enqueue(regular_priority)
+    assert priority_queue.search(0) == high_priority
+
+    with pytest.raises(IndexError):
+        priority_queue.search(100)
