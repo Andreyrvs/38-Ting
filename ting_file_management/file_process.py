ee4baf96420ba294bcb1c0d218174417ab089cf6
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 20:06:59 -0300

    feat: (Requisito 03) - cria o dict com os dados necessarios. Retorna os dados do processo

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,16 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+def process(path_file, instance):
+    file_txt = txt_importer(path_file)
+    data = {
+       "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_txt),
+        "linhas_do_arquivo": file_txt,
+    }
+
+    instance.enqueue(data)
+    return print(data, file=sys.stdout)
 
 def remove(instance):
     """Aqui irá sua implementação"""
