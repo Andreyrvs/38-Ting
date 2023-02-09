df006c46c40e9b204ab7b2aeb113ebb759bb585b
Andrey Visniewski <cortaesai@gmail.com>
2023-02-08 21:26:42 -0300

    feat: (Requisito 07) - Inicia logica

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    file = instance.queue()
+
+    a = [element["linhas_do_arquivo"] for element in file if word == element]
+    print("🔥🔥🔥🔥🔥", a)
+    # test = [{
+    #     "palavra": word,
+    #     "arquivo": file["nome_do_arquivo"],
+    #     "ocorrencias": [
+    #         {
+    #         "linha": 2
+    #         },
+    #         {
+    #         "linha": 7
+    #         }
+    #     ]
+    # }]
+    return a
 
 
 def search_by_word(word, instance):
