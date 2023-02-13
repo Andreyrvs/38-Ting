08eacaf7d359268e2a65c6bf4df7aaa4a3dc035a
Andrey Visniewski <cortaesai@gmail.com>
2023-02-13 09:43:01 -0300

    chore: (Requisito 07) - Conta a quantidades de linhas em que a palavra aparece

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,15 +5,25 @@ def exists_word(word, instance):
 
     for file_index in range(len(instance)):
         file = instance.search(file_index)
-        print('file: ', file)
 
         data = {
             "palavra": word,
             "arquivo": file["nome_do_arquivo"],
             "ocorrencias": list(),
         }
+        rows = 1
+
+        for row in file["linhas_do_arquivo"]:
+            if word_lower in row.lower():
+                data["ocorrencias"].append({"linha": rows})
+
+            rows += 1
+
+        if len(data["ocorrencias"]) > 0:
+            find_word.append(data)
 
     return find_word
+
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
